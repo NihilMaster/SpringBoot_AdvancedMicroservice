89ee6107c30d695114c8414ff5153a3ebc061a81
mateo.nihil <mateo.nihil@gmail.com>
2024-08-21 20:42:29 -0500

    5. Initial Books Microservice

---
+++ microservice-books/src/test/java/zzz/master/books/MicroserviceBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package zzz.master.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
