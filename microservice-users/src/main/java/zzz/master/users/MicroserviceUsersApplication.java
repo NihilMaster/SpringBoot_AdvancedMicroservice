5829a66521e3ecded81f23b0be6577a7b466dd1b
mateo.nihil <mateo.nihil@gmail.com>
2024-08-21 20:41:23 -0500

    4. Initial User Microservice

---
+++ microservice-users/src/main/java/zzz/master/users/MicroserviceUsersApplication.java
@@ -0,0 +1,13 @@
+package zzz.master.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUsersApplication.class, args);
+	}
+
+}
